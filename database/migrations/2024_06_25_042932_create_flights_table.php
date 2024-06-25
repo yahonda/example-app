309cff53fd14a8a6a308501b93a5067e7eda76f5
Yasuo Honda <yahonda@pingcap.com>
2024-06-25 13:47:57 +0900

    Add name and airline attributes

--- database/migrations/2024_06_25_042932_create_flights_table.php
+++ database/migrations/2024_06_25_042932_create_flights_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('flights', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('airline');
             $table->timestamps();
         });
     }
